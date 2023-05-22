50be6d6d6a3971147b8723f5ae0029a86b085640
ajihairi <hamzhyatinnov@gmail.com>
2023-05-22 18:51:02 +0700

    adding codes

--- LoginAppSwiftUI/ContentView.swift
+++ LoginAppSwiftUI/ContentView.swift
@@ -8,14 +8,80 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var username = ""
+    @State private var password = ""
+    @State private var wrongUsername: Float = 0
+    @State private var wrongPassword: Float = 0
+    @State private var loggedIn = false
+    @State private var buttonPressed = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            ZStack {
+                Color.blue
+                    .ignoresSafeArea(.all)
+                Circle()
+                    .scale(1.7)
+                    .foregroundColor(.white.opacity(0.15))
+                Circle()
+                    .scale(1.35)
+                    .foregroundColor(.white)
+                VStack {
+                    Text("Login")
+                        .font(.largeTitle)
+                        .bold()
+                        .padding()
+                    
+                    TextField("Username", text: $username)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                        .border(.red, width: CGFloat(wrongUsername))
+                    
+                    SecureField("Password", text: $password)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                        .border(.red, width: CGFloat(wrongPassword))
+                    
+                    Button("Login") {
+                        authenticateUser(username: username, password: password)
+                    }
+                    .foregroundColor(.white)
+                    .frame(width: 300, height: 50)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+                    .onLongPressGesture(minimumDuration: 0.1) { isPressed in
+                        buttonPressed = true
+                    } perform: {
+                        buttonPressed = false
+                    }
+                }
+                
+                if loggedIn {
+                    Text("You are logged in @\(username)")
+                        .foregroundColor(.white)
+                        .font(.title)
+                }
+            }
+            .navigationBarHidden(true)
         }
-        .padding()
+    }
+    
+    func authenticateUser(username: String, password: String) {
+        if username.lowercased() == "-" {
+            wrongUsername = 0
+            if password.lowercased() == "-" {
+                wrongPassword = 0
+                loggedIn = true
+            } else {
+                wrongPassword = 2
+            }
+        } else {
+            wrongUsername = 2
+        }
     }
 }
 
